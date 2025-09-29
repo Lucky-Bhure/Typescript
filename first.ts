b5922bcb3101162745206ea5577fd6ba2d569fbe
Lucky-Bhure <luckybhure19@gmail.com>
2025-09-29 14:55:36 +0530

    Type Annotation

---
+++ first.ts
@@ -0,0 +1,8 @@
+// Todo1: Addition of Two Numbers
+function sum(num1: number, num2:number):number {
+    return num1+num2;
+}
+console.log("Addition: ", sum(14,22));
+
+// Todo2: How to catch Errors & solve it?
+// Todo3: TS Configuration File
